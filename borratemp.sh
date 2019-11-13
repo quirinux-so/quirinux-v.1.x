76fe229c20bd0fbbe5957d0fb44b96339e11a43f
Quirinux <49002514+quirinux-so@users.noreply.github.com>
2019-11-13 14:28:45 +0100

    Update borratep.sh
    El programa sirve para borrar carpetas de temporales muy grandes que deja Systemback en el disco duro luego de la instalación / restauración de la ISO generada. Este Script fue desarrollado para Quirinux, pero se puede aplicar a cualquier otra distribución que no posea un script de similares características. En su actualización 1.2 se agregó la eliminación de carpetas de documentación y de archivos de instalación .deb, en un intento de útil ahorro de disco a la hora de generar una nueva imagen ISO con Systemback o cualquier otro script de remasterización, atendiendo al limite de 4 GB.

--- borratemp.sh
+++ borratemp.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
-# MÓDULO: BORRATEMP
-# Módulo que borra los temporales de instalación de Quirinux.
-#
-# Este código fuente forma parte del Sistema Operativo Quirinux V.1.0
-# y fue programado por Charlie Martínez
-# Abril de 2019, compartido bajo licencia GPLv3
-# Por alguna razón, Systemback no elimina una carpeta muy grande
-# e innecesaria luego de instalar. Este código se encargará de borrarla.
+#######################################################################
+#                                                                     #
+# Borratemp v.1.2                                                     #
+# Script que borra los temporales de instalación de Quirinux.         #
+#                                                                     #
+#######################################################################
+#                                                                     #
+# Por alguna razón, Systemback no borra unas carpetas muy grandes     #
+# que ya no tienen utilidad una vez instalado el sistema en el disco. #
+# Este programa sirve para cumplir con esa tarea.                     #
+#                                                                     #
+# Modo de uso:                                                        #
+#                                                                     #
+# 1) Otorgar permisos de ejecución                                    #
+# 2) Ejecutar con sudo ./borratemp.sh                                 #
+#                                                                     #
+# Este código viene incluido y configurado al inicio                  #
+# del Sistema Operativo Quirinux.                                     #
+#                                                                     #
+# Fue programado por Charlie Martínez en Abril de 2019.               #
+# Compartido bajo licencia GPLv3:                                     #
+#                                                                     #
+# Usted puede usar, modificar, copiar, compartir, redistribuir        #
+# este software, siempre y cuando respete los créditos del autor.     #
+#                                                                     #
+#######################################################################
 
+# Borrado de las carpetas residuales de Systemback:
 
 cd /lib/live/mount/rootfs/
 
@@ -17,5 +36,12 @@ sudo rm -rf filesystem.squashfs
 cd /lib/live/mount
 sudo rm -rf medium
 
+# Extra 1: elimina archivos de documentación:
 
+cd /usr/share/doc/
+sudo rm -rf *.*
 
+# Extra 2: elimina arhivos.deb de instalación
+
+cd /var/cache/apt/archives/
+sudo rm -rf *.deb
